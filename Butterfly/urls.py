ffc8a00fbd4e27fbe1c5a5505afcc3fa9143f657
MohammadNyundo <mohammadnyundo00122@gmail.com>
2025-03-13 22:08:23 +0300

    Updated templates and URLs after merge resolution

--- Butterfly/urls.py
+++ Butterfly/urls.py
@@ -17,20 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from ButterflyApp.views import custom_login
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     
     # Redirect root URL to the login page
-    path('', custom_login, name="home"),
+    path("", custom_login, name="home"),
 
     # Researcher (Regular User) URLs
-    path('butterflies/', include('ButterflyApp.urls')),
+    path("butterflies/", include("ButterflyApp.urls")),
 
     # Expert URLs (Separate for expert role)
-    path('experts/', include('ButterflyApp.experts_urls')),
+    path("experts/", include("ButterflyApp.experts_urls")),
 
     # Authentication URLs (Login, Logout, Password Reset, etc.)
-    path('accounts/login/', custom_login, name="login"),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/login/", custom_login, name="login"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
